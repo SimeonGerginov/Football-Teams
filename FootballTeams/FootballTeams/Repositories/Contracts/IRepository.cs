c446c06922b983de99f9934e9b7767fd1bdaa471
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-06-06 11:45:38 +0300

    Added EfCoreRepository

---
+++ FootballTeams/FootballTeams/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FootballTeams.Repositories.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+    }
+}
---
+++ FootballTeams/FootballTeams/Repositories/EfCoreRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FootballTeams.Data;
+using FootballTeams.Repositories.Contracts;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballTeams.Repositories
+{
+    public class EfCoreRepository<T> : IRepository<T> 
+        where T : class
+    {
+        private readonly DbSet<T> set;
+        private readonly FootballTeamsContext context;
+
+        public EfCoreRepository(FootballTeamsContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("Context is null !");
+            }
+
+            this.context = context;
+            this.set = this.context.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return this.set.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return this.set.AsEnumerable();
+        }
+
+        public void Add(T entity)
+        {
+            this.set.Add(entity);
+        }
+    }
+}
