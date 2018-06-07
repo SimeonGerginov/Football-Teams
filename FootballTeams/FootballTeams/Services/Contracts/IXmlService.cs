5936535a528419294932b0e3bb18689e8a6d2f03
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-06-07 11:47:06 +0300

    Added CreateDto methods

--- FootballTeams/FootballTeams/Services/Contracts/IXmlService.cs
+++ FootballTeams/FootballTeams/Services/Contracts/IXmlService.cs
@@ -5,5 +5,7 @@ namespace FootballTeams.Services.Contracts
     public interface IXmlService
     {
         void WriteTeamToXml(string fileName, Team team);
+
+        // Team ReadFromXml(string fileName);
     }
 }
